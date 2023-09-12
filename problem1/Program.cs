6e2fc20c1296605f361f7e366955026c16ad85e8
Refaat Elabd <RefaatElabd7@gmail.com>
2023-09-12 19:01:48 +0300

    Add project files.

---
+++ problem1/Program.cs
@@ -0,0 +1,22 @@
+﻿// See https://aka.ms/new-console-template for more information
+using problem1;
+using System;
+
+Console.WriteLine("Hello, World!");
+foreach (var list in Solution.GroupThePeople(new int[] { 3, 3, 3, 3, 3, 1, 3 }))
+{
+	foreach (var item in list)
+	{
+		Console.Write(item + " ");
+	}
+	Console.WriteLine();
+}
+
+foreach (var list in Solution.GroupThePeople(new int[] { 2, 1, 3, 3, 3, 2 }))
+{
+    foreach (var item in list)
+    {
+        Console.Write(item + " ");
+    }
+    Console.WriteLine();
+}
---
+++ problem1/Solution.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace problem1
+{
+    public class Solution
+    {
+        public static IList<IList<int>> GroupThePeople(int[] groupSizes)
+        {
+            var dic = new Dictionary<int, List<int>>();
+            var result = new List<IList<int>>();
+            for (int i = 0; i < groupSizes.Length; i++)
+            {
+                if (!dic.ContainsKey(groupSizes[i]))
+                {
+                    dic.Add(groupSizes[i], new List<int>(groupSizes[i]));
+                }
+                dic[groupSizes[i]].Add(i);
+
+                if(dic[groupSizes[i]].Count == groupSizes[i])
+                {
+                    result.Add(dic[groupSizes[i]]);
+                    dic.Remove(groupSizes[i]);
+                }
+            }
+
+            return result;
+        }
+    }
+}
