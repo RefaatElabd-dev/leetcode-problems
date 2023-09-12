755b4c0d24e392e06ddb7d296f8d4fb341207bc5
Refaat Elabd <RefaatElabd7@gmail.com>
2023-09-12 19:02:38 +0300

    1282. Group the People Given the Group Size They Belong To

--- problem1/Solution.cs
+++ problem1/Solution.cs
@@ -1,11 +1,4 @@
-﻿using System;
-using System.Collections;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace problem1
+﻿namespace problem1
 {
     public class Solution
     {
